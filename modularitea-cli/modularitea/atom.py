24fce57cb586a1eecf90998af7590d06549f5bcf
RizkiRF <riskiramadhan0397@gmail.com>
2018-05-06 16:40:21 +0700

    comment dir

--- modularitea-cli/modularitea/atom.py
+++ modularitea-cli/modularitea/atom.py
@@ -5,7 +5,7 @@ import json
 # http://stackoverflow.com/questions/5721529/running-python-script-as-root-with-sudo-what-is-the-username-of-the-effectiv
 user = os.getenv("SUDO_USER")
 
-USER_ATOMS_DIR = '/home/' + user + '/.modulaitea/atoms/'
+#USER_ATOMS_DIR = '/home/' + user + '/.modulaitea/atoms/'
 SYS_ATOMS_DIR = '/usr/share/modularitea/atoms/'
 
 
